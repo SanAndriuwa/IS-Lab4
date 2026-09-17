15f92fb6d58701288a9a98b6e48eb8e1861c9295
SanAndriuwa <37011046+SanAndriuwa@users.noreply.github.com>
2026-09-17 18:47:54 +0300

    Refactor image recognition script into reusable RBF function

--- vaizdo_atpazinimas.m
+++ vaizdo_atpazinimas.m
@@ -1,164 +1,40 @@
-close all
-clear all
-clc
-%% raidþiø pavyzdþiø nuskaitymas ir poþymiø skaièiavimas
-%% read the image with hand-written characters
-pavadinimas = 'train_data.png';
-pozymiai_tinklo_mokymui = pozymiai_raidems_atpazinti(pavadinimas, 8);
-%% Atpaþintuvo kûrimas
-%% Development of character recognizer
-% poþymiai ið celiø masyvo perkeliami á matricà
-% take the features from cell-type variable and save into a matrix-type variable
-P = cell2mat(pozymiai_tinklo_mokymui);
-% sukuriama teisingø atsakymø matrica: 11 raidþiø, 8 eilutës mokymui
-% create the matrices of correct answers for each line (number of matrices = number of symbol lines)
-T = [eye(11), eye(11), eye(11), eye(11), eye(11), eye(11), eye(11), eye(11)];
-% sukuriamas SBF tinklas duotiems P ir T sàryðiams
-% create an RBF network for classification with 13 neurons, and sigma = 1
-tinklas = newrb(P,T,0,1,13);
+function [predicted, atsakymas, tinklas, actualNeurons] = vaizdo_atpazinimas(P, T, P2, neuronLimit, spread)
+%VAIZDO_ATPAZINIMAS Train an RBF recognizer and classify test symbols.
+%
+% Inputs:
+%   P           - training features. One column = one training symbol.
+%   T           - correct training targets in one-hot form.
+%   P2          - test features. One column = one unknown symbol.
+%   neuronLimit - maximum number of hidden RBF neurons.
+%   spread      - RBF spread parameter.
+%
+% Outputs:
+%   predicted     - recognized digits as numbers 0...9.
+%   atsakymas     - recognized digits as a character string.
+%   tinklas       - trained RBF neural network.
+%   actualNeurons - actual number of hidden RBF neurons created by newrb.
+%
+% The function keeps the same main recognition logic as the original
+% laboratory example: newrb -> sim -> max.
 
-%% Tinklo patikra | Test of the network (recognizer)
-% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
-% estimate output of the network for unknown symbols (row, that were not used during training)
-P2 = P(:,12:22);
+%% 1. Create and train the RBF network.
+% goal = 0 means newrb tries to reduce the training error as much as it can.
+% neuronLimit is the MAXIMUM number of hidden neurons, not always the final one.
+tinklas = newrb(P, T, 0, spread, neuronLimit);
+actualNeurons = tinklas.layers{1}.size;
+
+%% 2. Run the trained network on unknown/test symbols.
+% Y2 has one column per test symbol and one row per output class.
 Y2 = sim(tinklas, P2);
-% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
-% find which neural network output gives maximum value
-[a2, b2] = max(Y2);
-%% Rezultato atvaizdavimas
-%% Visualize result
-% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
-% calculate the total number of symbols in the row
-raidziu_sk = size(P2,2);
-% rezultatà saugosime kintamajame 'atsakymas'
-% we will save the result in variable 'atsakymas'
-atsakymas = [];
-for k = 1:raidziu_sk
-    switch b2(k)
-        case 1
-            % the symbol here should be the same as written first symbol in your image
-            atsakymas = [atsakymas, 'A'];
-        case 2
-            atsakymas = [atsakymas, 'B'];
-        case 3
-            atsakymas = [atsakymas, 'C'];
-        case 4
-            atsakymas = [atsakymas, 'D'];
-        case 5
-            atsakymas = [atsakymas, 'E'];
-        case 6
-            atsakymas = [atsakymas, 'F'];
-        case 7
-            atsakymas = [atsakymas, 'G'];
-        case 8
-            atsakymas = [atsakymas, 'H'];
-        case 9
-            atsakymas = [atsakymas, 'I'];
-        case 10
-            atsakymas = [atsakymas, 'K'];
-        case 11
-            atsakymas = [atsakymas, 'J'];
-    end
-end
-% pateikime rezultatà komandiniame lange
-% show the result in command window
-disp(atsakymas)
-% % figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
-%% þodþio "KADA" poþymiø iðskyrimas 
-%% Extract features of the test image
-pavadinimas = 'test_kada.png';
-pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
 
-%% Raidþiø atpaþinimas
-%% Perform letter/symbol recognition
-% poþymiai ið celiø masyvo perkeliami á matricà
-% features from cell-variable are stored to matrix-variable
-P2 = cell2mat(pozymiai_patikrai);
-% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
-% estimating neuran network output for newly estimated features
-Y2 = sim(tinklas, P2);
-% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
-% searching which output gives maximum value
-[a2, b2] = max(Y2);
-%% Rezultato atvaizdavimas | Visualization of result
-% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
-% calculating number of symbols - number of columns
-raidziu_sk = size(P2,2);
-% rezultatà saugosime kintamajame 'atsakymas'
-atsakymas = [];
-for k = 1:raidziu_sk
-    switch b2(k)
-        case 1
-            atsakymas = [atsakymas, 'A'];
-        case 2
-            atsakymas = [atsakymas, 'B'];
-        case 3
-            atsakymas = [atsakymas, 'C'];
-        case 4
-            atsakymas = [atsakymas, 'D'];
-        case 5
-            atsakymas = [atsakymas, 'E'];
-        case 6
-            atsakymas = [atsakymas, 'F'];
-        case 7
-            atsakymas = [atsakymas, 'G'];
-        case 8
-            atsakymas = [atsakymas, 'H'];
-        case 9
-            atsakymas = [atsakymas, 'I'];
-        case 10
-            atsakymas = [atsakymas, 'K'];
-        case 11
-            atsakymas = [atsakymas, 'J'];
-    end
-end
-% pateikime rezultatà komandiniame lange
-% disp(atsakymas)
-figure(8), text(0.1,0.5,atsakymas,'FontSize',38), axis off
-%% þodþio "FIKCIJA" poþymiø iðskyrimas 
-%% extract features for next/another test image
-pavadinimas = 'test_fikcija.png';
-pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+%% 3. Choose the output neuron with the largest value.
+% b2 contains MATLAB indices 1...10.
+[~, b2] = max(Y2, [], 1);
 
-%% Raidþiø atpaþinimas
-% poþymiai ið celiø masyvo perkeliami á matricà
-P2 = cell2mat(pozymiai_patikrai);
-% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
-Y2 = sim(tinklas, P2);
-% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
-[a2, b2] = max(Y2);
-%% Rezultato atvaizdavimas
-% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
-raidziu_sk = size(P2,2);
-% rezultatà saugosime kintamajame 'atsakymas'
-atsakymas = [];
-for k = 1:raidziu_sk
-    switch b2(k)
-        case 1
-            atsakymas = [atsakymas, 'A'];
-        case 2
-            atsakymas = [atsakymas, 'B'];
-        case 3
-            atsakymas = [atsakymas, 'C'];
-        case 4
-            atsakymas = [atsakymas, 'D'];
-        case 5
-            atsakymas = [atsakymas, 'E'];
-        case 6
-            atsakymas = [atsakymas, 'F'];
-        case 7
-            atsakymas = [atsakymas, 'G'];
-        case 8
-            atsakymas = [atsakymas, 'H'];
-        case 9
-            atsakymas = [atsakymas, 'I'];
-        case 10
-            atsakymas = [atsakymas, 'K'];
-        case 11
-            atsakymas = [atsakymas, 'J'];
-    end
-end
-% pateikime rezultatà komandiniame lange
-% disp(atsakymas)
-figure(9), text(0.1,0.5,atsakymas,'FontSize',38), axis off
+%% 4. Convert MATLAB output indices to digit labels 0...9.
+predicted = b2 - 1;
 
+% Output 1 = digit 0, output 2 = digit 1, ..., output 10 = digit 9.
+symbols = '0123456789';
+atsakymas = symbols(b2);
+end
